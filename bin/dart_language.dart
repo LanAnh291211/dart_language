be96959a57d15c73df4c8081bcb017d17c96478c
LanAnh291211 <100055407+LanAnh291211@users.noreply.github.com>
2022-06-13 20:00:01 +0700

    Update dart_language.dart

--- bin/dart_language.dart
+++ bin/dart_language.dart
@@ -1,24 +1,23 @@
-abstract class Abtract {
-    // Creating Abstract Methods
-    void think();
-    void act();
+import 'dart:async';
+
+void createCount() async {
+  var count1 = await getCount1();
+  print(' $count1');
+  var count2 = await getCount2();
+  print(' $count2');
 }
- 
-class AbtractExample extends Abtract{
-    @override
-    void think()
-    {
-        print("Hate");
-    }
- 
-    @override
-    void act()
-    {
-        print("Love");
-    }
+
+Future<String> getCount1() => Future.delayed(Duration(seconds: 6), () => 'Anh có đánh rơi nhịp nào không :v ');
+
+Future<String> getCount2() => Future.delayed(Duration(seconds: 3), () => 'Không em :)))');
+
+void countSeconds(s) {
+  for (var i = 1; i <= s; i++) {
+    Future.delayed(Duration(seconds: i), () => print(i));
+  }
 }
+
 void main() {
-   AbtractExample abtract = new AbtractExample();
-    abtract.think();
-    abtract.act();
+  countSeconds(5);
+  createCount();
 }
