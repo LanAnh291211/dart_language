552c9767f607470b85e960f470bb99a8058d8e54
LanAnh291211 <100055407+LanAnh291211@users.noreply.github.com>
2022-06-14 08:23:37 +0700

    main

--- bin/dart_language.dart
+++ bin/dart_language.dart
@@ -1,24 +1,53 @@
-abstract class Abtract {
-    // Creating Abstract Methods
-    void think();
-    void act();
+//trong vài trường hợp việc tạo 1 class có vẻ dư thừa, thay vì tạo 1 class ta có tạo 1 extension 
+//là một cách để thêm chức năng vào các thư viện hiện có
+// extension <'Tên Extension'> on <'Class cần extends'> {}
+extension Extensions on String {
+  String concat(String word) => '$this $word';
+
+  // DOCUMENTATION IS SUPPORTED: Concatenates two strings with a space in between.
+
 }
- 
-class AbtractExample extends Abtract{
-    @override
-    void think()
-    {
-        print("Hate");
-    }
- 
-    @override
-    void act()
-    {
-        print("Love");
-    }
+class Pet {
+  Pet(this.name, this.id);
+  
+  int id;
+  String name;
+
+  void sound() {
+    print('gaugau');
+  }
 }
-void main() {
-   AbtractExample abtract = new AbtractExample();
-    abtract.think();
-    abtract.act();
+//class Cat kế thừa class Pet
+class Cat extends Pet {
+  // use the 'super' keyword to interact with 
+  // the super class of Cat
+  Cat(String name, int id) : super(name, id);
+  
+  void sound() {
+    print('meomeo');
+  }
+  
+}
+
+//class Dog kế thừa class Pet
+class Dog extends Pet {
+  
+  Dog(String name, int id) : super(name, id);
+  
+  void sound() {
+    print('grugru');
+  }
+  
+}
+
+
+main() {
+
+  String one = 'dog';
+  one.concat('cat');
+   var cat = Cat("Miu",1);
+  var dog = Dog("Nick", 2);
+  
+  dog.sound();
+  cat.sound();
 }
