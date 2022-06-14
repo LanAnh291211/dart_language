1a5e14ad5a0b6af01f7d4913ffdd92bbe83e5101
LanAnh291211 <100055407+LanAnh291211@users.noreply.github.com>
2022-06-14 17:53:13 +0700

    with

--- bin/dart_language.dart
+++ bin/dart_language.dart
@@ -1,53 +1,29 @@
-//trong vài trường hợp việc tạo 1 class có vẻ dư thừa, thay vì tạo 1 class ta có tạo 1 extension 
-//là một cách để thêm chức năng vào các thư viện hiện có
-// extension <'Tên Extension'> on <'Class cần extends'> {}
-extension Extensions on String {
-  String concat(String word) => '$this $word';
-
-  // DOCUMENTATION IS SUPPORTED: Concatenates two strings with a space in between.
-
-}
-class Pet {
-  Pet(this.name, this.id);
-  
-  int id;
-  String name;
-
-  void sound() {
-    print('gaugau');
+// With được sử dụng để bao gồm các Mixin. Mixin là một loại cấu trúc khác, chỉ có thể được sử dụng với từ khóa with.
+// Mixin là một cách sử dụng lại các phương thức của một lớp trong nhiều phân cấp lớp. 
+//Mixin có thể được hiểu là các lớp trừu tượng được sử dụng để sử dụng lại các phương thức trong các lớp khác nhau có các chức năng / thuộc tính tương tự.
+mixin Pet {
+  void show(){
+    print('he');
   }
 }
-//class Cat kế thừa class Pet
-class Cat extends Pet {
-  // use the 'super' keyword to interact with 
-  // the super class of Cat
-  Cat(String name, int id) : super(name, id);
-  
-  void sound() {
-    print('meomeo');
+ 
+// mixin with name temp
+mixin Animal {
+  void sound(){
+    print("gâu");
   }
-  
 }
-
-//class Dog kế thừa class Pet
-class Dog extends Pet {
-  
-  Dog(String name, int id) : super(name, id);
-  
-  void sound() {
-    print('grugru');
+ 
+// mixin type used with keyword
+class Dog with Pet, Animal{
+  @override
+  void show(){
+    print('có thể ghi đè nếu cần');
   }
-  
 }
-
-
-main() {
-
-  String one = 'dog';
-  one.concat('cat');
-   var cat = Cat("Miu",1);
-  var dog = Dog("Nick", 2);
-  
+ 
+void main(){
+  var dog = Dog();
+  dog.show();
   dog.sound();
-  cat.sound();
 }
