6f7ec3b8d7e015fdf49a6c55eeed9fd3d991b6f8
LanAnh291211 <100055407+LanAnh291211@users.noreply.github.com>
2022-06-14 09:53:24 +0700

    mixin

--- bin/dart_language.dart
+++ bin/dart_language.dart
@@ -1,24 +1,50 @@
-abstract class Abtract {
-    // Creating Abstract Methods
-    void think();
-    void act();
-}
+//  //mixin không có constructor nên không thể tạo đối tượng.
+// mixin chỉ có thể sử dụng để implements hoặc with chứ không thể extends
+// mixin có thể giới hạn những class nào được phép sử dụng code của mình bằng từ khóa mixin on
+ class Dog {
+ 	bool isRun() {
+ 		return true;
+ 	}
+ 	
+ 	void sound() {
+ 		print('gâu gâu');
+ 	}
+ }
  
-class AbtractExample extends Abtract{
-    @override
-    void think()
-    {
-        print("Hate");
-    }
+ class Cat {
+ 	bool isClimb() {
+ 		return true;
+ 	}
+ 	
+ 	void sound() {
+ 		print('Meo meo');
+ 	}
+ }
  
-    @override
-    void act()
-    {
-        print("Love");
-    }
-}
-void main() {
-   AbtractExample abtract = new AbtractExample();
-    abtract.think();
-    abtract.act();
-}
+ class Chicken() {
+ 	bool isRunInWater() {
+ 		return true;
+ 	}
+ 	
+ 	void sound() {
+ 		print('cục tác');
+ 	}
+ }
+ 
+ mixin Mixin on Chicken {
+ 	
+ 	
+ 	bool isHaveWing() {
+ 		return true;
+ 	}
+ }
+ 
+ class Bird extends Dog with Cat, Chicken {}
+ 
+ main() {
+ 	Bird myBird = Bird();
+ 	myBird.isRun(); // true
+ 	myBird.isClimb(); // true
+ 	myBird.isClimb(); // true
+ 	myBird.sound(); // This is a ship
+ }
