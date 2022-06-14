d1e2cc8bacc8d33be7f566e0b1e572d000348958
LanAnh291211 <100055407+LanAnh291211@users.noreply.github.com>
2022-06-14 17:47:27 +0700

    anc

--- bin/dart_language.dart
+++ bin/dart_language.dart
@@ -1,53 +1,28 @@
-//trong vài trường hợp việc tạo 1 class có vẻ dư thừa, thay vì tạo 1 class ta có tạo 1 extension 
-//là một cách để thêm chức năng vào các thư viện hiện có
-// extension <'Tên Extension'> on <'Class cần extends'> {}
-extension Extensions on String {
-  String concat(String word) => '$this $word';
-
-  // DOCUMENTATION IS SUPPORTED: Concatenates two strings with a space in between.
-
-}
 class Pet {
-  Pet(this.name, this.id);
-  
-  int id;
-  String name;
-
-  void sound() {
-    print('gaugau');
+  void show() {
+    print('haha');
   }
 }
-//class Cat kế thừa class Pet
-class Cat extends Pet {
-  // use the 'super' keyword to interact with 
-  // the super class of Cat
-  Cat(String name, int id) : super(name, id);
-  
-  void sound() {
-    print('meomeo');
+ //được sử dụng để buộc các function phải được định nghĩa lại để triển khai một giao diện.
+class Dog implements Pet {
+  // // phương thức kế thừa
+  @override
+  void show() {
+    print('hoho');
   }
-  
-}
-
-//class Dog kế thừa class Pet
-class Dog extends Pet {
-  
-  Dog(String name, int id) : super(name, id);
-  
-  void sound() {
-    print('grugru');
+  // tạo phương thức
+  void sound(){
+    print('gâu gâu');
   }
-  
 }
-
-
-main() {
-
-  String one = 'dog';
-  one.concat('cat');
-   var cat = Cat("Miu",1);
-  var dog = Dog("Nick", 2);
+ 
+void main() {
   
-  dog.sound();
-  cat.sound();
+  var kin = Dog();
+
+  // Gọi phương thức của lớp Pet
+  // sử dụng cá thể lớp Dog
+  kin.show();
+  // // Gọi phương thức của lớp Pet 
+  kin.sound();
 }
